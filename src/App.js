92070faf194de874ceae47c9661e943918797b8a
Kauarafaldini <kauarsamtos0@gmail.com>
2023-06-22 10:26:45 -0300

    Alteração na estrutura do formulario

--- src/App.js
+++ src/App.js
@@ -5,72 +5,122 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Formulario de Feedback</h1>
+      <h1>Cadastro de cliente</h1>
 
+      <label>Nome: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='FIRTS NAME'
-      />
+      name='text'
+      placeholder='NOME'
+      required ="required"
+      /><br/>
 
+      <label>Sobrenome: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='lAST NAME'
-      />
+      name='text'
+      placeholder='SOBRENOME'
+      required
+      /><br/>
 
+      <label>Usuario: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='USERNAME'
-      />  
+      name='text'
+      placeholder='NOME DE USUARIO'
+      required
+      /><br/>
 
+      <label>E-mail: </label>
+      <br/>
       <input 
-      type='text'
-      placeholder='EMAIL'
-      />
+      type='email'
+      name='email'
+      placeholder='E-MAIL'
+      required
+      /><br/>
 
+      <label>Telefone: </label>
+      <br/>
       <input
       type='text'
-      placeholder='PHONE NUMBER'
+      name='fone'
+      placeholder='TELEFONE OU CELULAR'
+      required
+      /><br/>
+
+      <input 
+      className='button'
+      type='submit' 
+      value="ENVIAR"
       />
 
       <input 
       className='button'
-      type='button' 
-      value="SEND"
-      onClick={0}
+      type='reset' 
+      value="LIMPAR"
       />
 
-      <h1>Formulario de Busca</h1>
+      <h1>Buscar cliente</h1>
 
+      <label>Nome: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='FIRTS NAME'
-      />
+      name='text'
+      placeholder='NOME'
+      required
+      /><br/>
 
+      <label>Sobrenome: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='lAST NAME'
-      />
+      name='text'
+      placeholder='SOBRENOME'
+      required
+      /><br/>
 
+      <label>Usuario: </label>
+      <br/>
       <input 
       type='text'
-      placeholder='USERNAME'
-      />  
+      name='text'
+      placeholder='NOME DE USUARIO'
+      required
+      /><br/>
 
+      <label>E-mail: </label>
+      <br/>
       <input 
-      type='text'
-      placeholder='EMAIL'
-      />
+      type='email'
+      name='email'
+      placeholder='E-MAIL'
+      required
+      /><br/>
 
+      <label>Telefone: </label>
+      <br/>
       <input
       type='text'
-      placeholder='PHONE NUMBER'
+      name='fone'
+      placeholder='TELEFONE OU CELULAR'
+      required
+      /><br/>
+
+      <input 
+      className='button'
+      type='submit' 
+      value="ENVIAR"
       />
 
       <input 
       className='button'
-      type='button' 
-      value="SEARCH"
-      onClick={0}
+      type='reset' 
+      value="LIMPAR"
       />
 
     </div>
