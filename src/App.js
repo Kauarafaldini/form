11f3fd976cc8b4c4d996b42fe5b5aced54a22ea1
Kauarafaldini <kauarsamtos0@gmail.com>
2023-06-22 11:33:59 -0300

    Criação de constante

--- src/App.js
+++ src/App.js
@@ -1,67 +1,95 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
 
-  const
+  const [cliente, setCliente] = useState({
+    nome:'',
+    sobrenome:'',
+    usuario:'',
+    email:'',
+    fone:''
+  });
+
+  const vlInput = e => setCliente({...cliente, [e.target.name]: e.target.value});
+
+  const enviarmsg = (e) => {
+
+    e.preventDefault();
+
+    console.log(`Nome: ${cliente.name}`);
+    console.log(`Nome: ${cliente.sobrenome}`);
+    console.log(`Nome: ${cliente.usuario}`);
+    console.log(`Nome: ${cliente.email}`);
+    console.log(`Nome: ${cliente.fone}`);
+  }
   
   return (
     <div className="App">
 
-      <h2>Cadastro de cliente</h2>
+      <form onSubmit={enviarmsg}>
 
-      <label>Nome: </label>
-      <br/>
-      <input 
-      type='text'
-      name='text'
-      placeholder='NOME'
-      required ="required"
-      /><br/>
+        <h2>Cadastro de cliente</h2>
 
-      <label>Sobrenome: </label>
-      <br/>
-      <input 
-      type='text'
-      name='text'
-      placeholder='SOBRENOME'
-      required
-      /><br/>
+        <label>Nome: </label>
+        <br/>
+        <input 
+        type='text'
+        name='nome'
+        placeholder='NOME'
+        required
+        onChange={vlInput}
+        /><br/>
 
-      <label>Usuario: </label>
-      <br/>
-      <input 
-      type='text'
-      name='text'
-      placeholder='NOME DE USUARIO'
-      required
-      /><br/>
+        <label>Sobrenome: </label>
+        <br/>
+        <input 
+        type='text'
+        name='sobrenome'
+        placeholder='SOBRENOME'
+        required
+        onChange={vlInput}
+        /><br/>
 
-      <label>E-mail: </label>
-      <br/>
-      <input 
-      type='email'
-      name='email'
-      placeholder='E-MAIL'
-      required
-      /><br/>
+        <label>Usuario: </label>
+        <br/>
+        <input 
+        type='text'
+        name='usuario'
+        placeholder='NOME DE USUARIO'
+        required
+        onChange={vlInput}
+        /><br/>
 
-      <label>Telefone: </label>
-      <br/>
-      <input
-      type='text'
-      name='fone'
-      placeholder='TELEFONE OU CELULAR'
-      required
-      /><br/>
+        <label>E-mail: </label>
+        <br/>
+        <input 
+        type='email'
+        name='email'
+        placeholder='E-MAIL'
+        required
+        onChange={vlInput}
+        /><br/>
 
-      <button
-      className='button_enviar'
-      type='submit'>ENVIAR</button>
+        <label>Telefone: </label>
+        <br/>
+        <input
+        type='text'
+        name='fone'
+        placeholder='TELEFONE OU CELULAR'
+        required
+        onChange={vlInput}
+        /><br/>
 
-      <button
-      className='button_limpar'
-      type='reset'>LIMPAR</button>
+        <button
+        className='button_enviar'
+        type='submit'>ENVIAR</button>
+
+        <button
+        className='button_limpar'
+        type='reset'>LIMPAR</button>
+      
+      </form>
 
       <h2>Buscar cliente</h2>
 
@@ -112,7 +140,7 @@ function App() {
 
       <button
       className='button_enviar'
-      type='submit'>ENVIAR</button>
+      type='submit'>BUSCAR</button>
 
       <button
       className='button_limpar'
